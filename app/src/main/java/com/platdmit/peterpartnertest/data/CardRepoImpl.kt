ae972d8394d69ceed1a7dfee7e0b85b8d17b3fcf
DmitriyPlatonov <platdm-it@yandex.ru>
2020-07-31 11:17:58 +0300

    fix| fun in cardRepo

--- app/src/main/java/com/platdmit/peterpartnertest/data/CardRepoImpl.kt
+++ app/src/main/java/com/platdmit/peterpartnertest/data/CardRepoImpl.kt
@@ -1,22 +1,20 @@
 package com.platdmit.peterpartnertest.data
 
-import com.platdmit.peterpartnertest.domain.converters.CardConverter
-import com.platdmit.peterpartnertest.domain.model.Card
-import com.platdmit.peterpartnertest.domain.repositories.CardRepo
-import com.platdmit.peterpartnertest.domain.repositories.api.ApiCardRepo
 import com.platdmit.peterpartnertest.data.retrofit.models.ApiCard
 import com.platdmit.peterpartnertest.data.retrofit.models.ApiTransaction
-import com.platdmit.peterpartnertest.data.room.dao.BaseDao
 import com.platdmit.peterpartnertest.data.room.dao.CardDao
 import com.platdmit.peterpartnertest.data.room.dao.TransactionDao
 import com.platdmit.peterpartnertest.data.room.entity.DbCard
 import com.platdmit.peterpartnertest.data.room.entity.DbTransaction
+import com.platdmit.peterpartnertest.domain.converters.CardConverter
 import com.platdmit.peterpartnertest.domain.converters.TransactionConverter
+import com.platdmit.peterpartnertest.domain.model.Card
 import com.platdmit.peterpartnertest.domain.model.Transaction
+import com.platdmit.peterpartnertest.domain.repositories.CardRepo
+import com.platdmit.peterpartnertest.domain.repositories.api.ApiCardRepo
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
-import java.lang.NullPointerException
 
 class CardRepoImpl(
     private val apiCardRepo: ApiCardRepo<ApiCard>,
@@ -30,8 +28,7 @@ class CardRepoImpl(
             try {
                 val card = cardConverter.fromDbToDomain(cardDao.getCard(number)!!)
                 transactionDao.getTransactions(number)?.let { dbTransactions ->
-                    card.transaction_history =
-                        dbTransactions.map { transactionConverter.fromDbToDomain(it) }
+                    card.transaction_history = dbTransactions.map { transactionConverter.fromDbToDomain(it) }
                 }
                 emitter.onSuccess(card)
             } catch (e: NullPointerException) {
@@ -68,26 +65,30 @@ class CardRepoImpl(
     override fun refreshCards(): Completable {
         return Completable.create{success ->
             apiCardRepo.getCards()
-                .onErrorComplete{
+                .onErrorComplete {
                     success.onError(Throwable("Fall"))
                     true
                 }
                 .subscribe { apiCards ->
                     cardDao.delCards()
-                    apiCards.forEach {
-                        val dbCard = cardConverter.fromApiToDb(it)
-                        val dbTransactions = it.transaction_history?.map {
-                                transaction -> transactionConverter.fromApiToDb(transaction, it.card_number)
-                        }
-                        dbCard.let { card ->
-                            cardDao.insert(card)
-                        }
-                        dbTransactions?.let { transactions ->
-                            transactionDao.insertList(transactions)
-                        }
-                    }
-                success.onComplete()
-            }
+                    saveApiModelToDb(apiCards)
+                    success.onComplete()
+                }
         }.subscribeOn(Schedulers.io())
+    }
+
+    private fun saveApiModelToDb(apiCards : List<ApiCard>){
+        apiCards.forEach {
+            val dbCard = cardConverter.fromApiToDb(it)
+            val dbTransactions = it.transaction_history?.map {
+                    transaction -> transactionConverter.fromApiToDb(transaction, it.card_number)
+            }
+            dbCard.let { card ->
+                cardDao.insert(card)
+            }
+            dbTransactions?.let { transactions ->
+                transactionDao.insertList(transactions)
+            }
+        }
     }
 }
